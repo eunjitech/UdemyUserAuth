24591dc69f0c7559e7e89402d8aa0cc561c10482
eunji Lee <eunjitech@gmail.com>
2023-05-22 14:47:41 +0900

    11-184: http에 token 추가

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-user-auth-4cb68-default-rtdb.firebaseio.com/message.json?auth=" +
+          token
+      )
+      .then((res) => {
+        setFetchedMessage(res.data);
+      }); //api에 token을 추가해 권한 사용
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +34,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
